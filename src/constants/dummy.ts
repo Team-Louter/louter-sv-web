4f22f16e86e4604c1260206d4fa5989707598b8e
전수안 <dgsw.ahn@dgsw.hs.kr>
2026-02-01 15:56:00 +0900

    feat: 게시글 컴포넌트 구현

--- src/constants/dummy.ts
+++ src/constants/dummy.ts
@@ -1,3 +1,4 @@
+import type { Post } from '@/types/post';
 import type { EventInput } from '@fullcalendar/core';
 
 export const dummyEvents: EventInput[] = [
@@ -5,3 +6,27 @@ export const dummyEvents: EventInput[] = [
   { title: '점심약속', date: '2026-02-10' },
   { title: '발표', date: '2026-02-15' }
 ];
+
+export const dummyPosts: Post[] = [
+    {
+      id: 1,
+      title: '전수안의 리액트 네이티브에서 생존하기',
+      viewCount: 361_523
+    },
+    {
+      id: 2,
+      title: '라우터에서 살아남는 법',
+      viewCount: 217_321
+    },
+    {
+      id: 3,
+      title: '상태 관리 지옥에서 탈출하기',
+      viewCount: 189_004
+    },
+    {
+      id: 4,
+      title: '이도연의 키 작아지는 법',
+      viewCount: 111_321
+    }
+  ];
+  
