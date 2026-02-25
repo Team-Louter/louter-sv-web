0355309b17eeb94f03dbdf862b052787e2fb4d6a
전수안 <dgsw.ahn@dgsw.hs.kr>
2026-02-25 23:32:40 +0900

    feat: 내 최신글, 인기글 get

--- src/types/post.d.ts
+++ src/types/post.d.ts
@@ -1,39 +1,71 @@
-export interface MainPost {
-  id: number;
-  title: string;
-  viewCount: number;
-}
-  
-export type Post = {
-  id: number;
-  category: string;
-  tag: string;
-  title: string;
-  content: string;
-  views: number;
-  likes: number;
-  comments: number;
-  author: string;
+// 게시글 하나 타입
+export interface Post {
+  commentCount: number;
   createdAt: string;
-  isPinned: boolean;
-  isHearted: boolean;
-}
-
-export interface postProps {
-  post: Post;
-  selectedCategory: string;
-}
-
-export interface Comment {
-  id: number;
+  likeCount: number;
+  postCategory: string;
   postId: number;
-  author: string; 
-  content: string;
+  postTitle: string;
+  viewers: number;
+}
+
+// 정렬 타입
+export interface Sort {
+  empty: boolean;
+  sorted: boolean;
+  unsorted: boolean;
+}
+
+// 페이지 정보
+export interface Pageable {
+  offset: number;
+  pageNumber: number;
+  pageSize: number;
+  paged: boolean;
+  sort: Sort;
+  unpaged: boolean;
+}
+
+// 전체 응답 타입
+export interface MyPost {
+  content: Post[];
+  empty: boolean;
+  first: boolean;
+  last: boolean;
+  number: number;
+  numberOfElements: number;
+  pageable: Pageable;
+  size: number;
+  sort: Sort;
+  totalElements: number;
+  totalPages: number;
+}
+
+export interface PostFile {
+  fileId: number;
+  fileUrl: string;
+  fileName: string;
+  fileType: string;
+  fileSize: number;
   createdAt: string;
 }
 
-export type commentProps = {
-  comment: Comment;
+export interface HotPost {
+  postId: number;
+  userName: string;
+  userProfileImageUrl: string;
+  postTitle: string;
+  postContent: string;
+  category: string;
+  createdAt: string;
+  isAnonymous: boolean;
+  viewers: number;
+  likeCount: number;
+  commentCount: number;
+  isHearted: boolean;
+  pinned: boolean;
+  tag: string;
+  files: PostFile[];
 }
 
 export type mainProps = {
