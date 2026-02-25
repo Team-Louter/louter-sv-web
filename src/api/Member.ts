cec34d092cc71b62d7a4c7f0517dcd133e280d78
전수안 <dgsw.ahn@dgsw.hs.kr>
2026-02-26 05:11:57 +0900

    refactor: 주석 삽입

--- src/api/Member.ts
+++ src/api/Member.ts
@@ -1,6 +1,7 @@
 import type { Member } from "@/types/member";
 import instance from "./Axios";
 
+// 멤버 가져오기
 export const getMember = async (gen: string): Promise<Member[]> => {
     const response = await instance.get<Member[]>("/main/members", {
         params: {
