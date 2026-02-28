8b2bd350cf1f56f65327d84dc3dcb17bdf19ffa6
hyxx-su <legendary9899@dgsw.hs.kr>
2026-03-01 00:21:42 +0900

    feat : 구글 회원가입 화면 구현

--- src/pages/SignupGoogle/index.tsx
+++ src/pages/SignupGoogle/index.tsx
@@ -1,5 +1,71 @@
-export default function SignupGoogle() {
-    return (
-        <div>회원가입 스텝 2 화면</div>
-    )
+import { useNavigate } from 'react-router-dom';
+import * as S from './style';
+import LogoSvg from '@/assets/AuthImg/AuthLogo.svg';
+
+function SignupGoogle() {
+  const navigate = useNavigate();
+
+  const handleTest = () => {
+    navigate('/');
+  };
+
+  const handleSignin = () => {
+    navigate('/auth/signin');
+  };
+
+  return (
+    <S.Container>
+      <S.SignupContainer>
+        <S.AuthMainImgContainer />
+        <S.AuthContent>
+          <S.LogoImg src={LogoSvg} alt="Logo" />
+
+          <S.GoogleContent>
+            <S.SocialTitle>학생 개인정보 입력</S.SocialTitle>
+          </S.GoogleContent>
+
+          <S.Line />
+
+          <S.SignupForm>
+            <S.Input type="text" placeholder="학번" required />
+            <S.Input type="text" placeholder="이름" required />
+            <S.Input type="text" placeholder="동아리 코드" required />
+            <S.Inputgap />
+            <S.SignupButton type="button" onClick={handleTest}>
+              회원가입
+            </S.SignupButton>
+          </S.SignupForm>
+
+          <S.SignupText>
+            이미 계정이 있으신가요?{' '}
+            <S.SignupLink onClick={handleSignin}>로그인</S.SignupLink>
+          </S.SignupText>
+
+          <S.Line />
+
+          <S.PolicyText>
+            The Google{' '}
+            <S.PolicyLink
+              href="https://policies.google.com/privacy"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Privacy Policy
+            </S.PolicyLink>{' '}
+            and{' '}
+            <S.PolicyLink
+              href="https://policies.google.com/terms"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Terms of Service
+            </S.PolicyLink>{' '}
+            apply.
+          </S.PolicyText>
+        </S.AuthContent>
+      </S.SignupContainer>
+    </S.Container>
+  );
 }
+
+export default SignupGoogle;
