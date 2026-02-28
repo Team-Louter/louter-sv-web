0b57fd718c69581e5d772ac3a254a7042ec6a233
hyxx-su <legendary9899@dgsw.hs.kr>
2026-02-28 21:16:45 +0900

    feat : Topbar 스크롤 hidden

--- src/layout/Layout/index.tsx
+++ src/layout/Layout/index.tsx
@@ -1,15 +1,25 @@
-import { Outlet } from "react-router-dom";
-import * as S from "./style"
-import Topbar from "@/components/common/Topbar";
+import { useRef, useState } from 'react';
+import { Outlet } from 'react-router-dom';
+import * as S from './style';
+import Topbar from '@/components/common/Topbar';
 
 function Layout() {
+  const [hidden, setHidden] = useState(false);
+  const lastScrollY = useRef(0);
+
+  const handleScroll = (e: React.UIEvent<HTMLDivElement>) => {
+    const currentY = e.currentTarget.scrollTop;
+    setHidden(currentY > lastScrollY.current && currentY > 60);
+    lastScrollY.current = currentY;
+  };
+
   return (
-    <S.Container>
+    <S.Container onScroll={handleScroll}>
       <S.HeaderSpacer />
-      <Topbar />
+      <Topbar hidden={hidden} />
       <Outlet />
     </S.Container>
-  )
+  );
 }
 
 export default Layout;
